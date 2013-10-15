3645b9a1231270c5fb6810d8382edfa8426400b1
syn cryptic <syn@foo.com>
2013-10-15 14:31:15 -0700

    Updated __all__ so that pydoc functions correctly with itertools imports

--- libs/looper/iterutil.py
+++ libs/looper/iterutil.py
@@ -1,8 +1,71 @@
 #!/usr/bin/env python
 '''
 Itertools extension for generating large and complex datasets using generators.
+
+    Infinite iterators:
+    count([n]) --> n, n+1, n+2, ...
+    cycle(p) --> p0, p1, ... plast, p0, p1, ...
+    repeat(elem [,n]) --> elem, elem, elem, ... endlessly or up to n times
+    repeat_f(f, n=None, args=[], kwargs={}) -> f(*args,**kwargs), f(*args,**kwargs), ... endlessly or up to n times
+    
+    Iterators terminating on the shortest input sequence:
+    chain(p, q, ...) --> p0, p1, ... plast, q0, q1, ... 
+    compress(data, selectors) --> (d[0] if s[0]), (d[1] if s[1]), ...
+    dropwhile(pred, seq) --> seq[n], seq[n+1], starting when pred fails
+    groupby(iterable[, keyfunc]) --> sub-iterators grouped by value of keyfunc(v)
+    ifilter(pred, seq) --> elements of seq where pred(elem) is True
+    ifilterfalse(pred, seq) --> elements of seq where pred(elem) is False
+    islice(seq, [start,] stop [, step]) --> elements from
+           seq[start:stop:step]
+    imap(fun, p, q, ...) --> fun(p[0], q[0]), fun(p[1], q[1]), ...
+    kwimap(fun, p, foo=q) -> fun(p[0], foo=q[0]), fun(p[0], foo=q[0]), ...
+    starmap(fun, seq) --> fun(*seq[0]), fun(*seq[1]), ...
+    tee(it, n=2) --> (it1, it2 , ... itn) splits one iterator into n
+    takewhile(pred, seq) --> seq[0], seq[1], until pred fails
+    izip(p, q, ...) --> (p[0], q[0]), (p[1], q[1]), ... 
+    izip_longest(p, q, ...) --> (p[0], q[0]), (p[1], q[1]), ... 
+    concat(p,q,...) --> (p[0]+q[0]), (p[1]+q[1]), ... 
+    dict_zip(foo=p,bar=q) {"foo": p[0], "bar": q[0]}, {"foo": p[1], "bar": q[1]}, ... 
+    
+    Combinatoric generators:
+    product(p, q, ... [repeat=1]) --> cartesian product
+    dict_product(foo=p,bar=q) --> cartesian product: {"foo": p[x], "bar": q[y]}
+    permutations(p[, r])
+    combinations(p, r)
+    combinations_with_replacement(p, r)
+
+    Utility generators:
+    readfiles(file_a,file_b,...) --> reads file_a line by line, then file_a line by line...
 '''
 from itertools import *
+__all__ = '''
+chain
+combinations
+combinations_with_replacement
+compress
+count
+cycle
+dropwhile
+groupby
+ifilter
+ifilterfalse
+imap
+islice
+izip
+izip_longest
+permutations
+product
+starmap
+repeat
+takewhile
+concat
+readfiles
+dict_product
+dict_zip
+kwimap
+repeat_f
+'''.strip().split('\n')
+
 
 def concat(*args):
     '''
@@ -43,7 +106,7 @@ def readfiles(*filenames):
 
         #prints file1, then file2
     '''
-    for path in args:
+    for path in filenames:
         for line in open(path,"rb"):
             yield line.strip()
 
@@ -80,7 +143,7 @@ def dict_product(d=None,**kwargs):
             ret[k[x]] = y[x]
         yield ret
 
-def dict_zip(d=None, **kwargs):
+def dict_zip(**kwargs):
     '''
     A version of itertools.izip for dictionaries
 
@@ -89,27 +152,50 @@ def dict_zip(d=None, **kwargs):
 
     Example:
         A = {"a":[1,2],"b":['x','y']}
-        r_1 = util.hash_product(A)
-        r_2 = util.hash_product(a=[1,2], b=['x','y'])
-        # r_1 == r_2
+        r = util.hash_product(a=[1,2], b=['x','y'])
         
-        pprint([x for x in r_1])
+        pprint([x for x in r])
 
         #OUTPUT    
         [{'a': 1, 'b': 'x'},
          {'a': 2, 'b': 'y'}]
 
     '''
-    if d is None:
-        d = kwargs
-    k = d.keys()
-    for y in izip(*d.values()):
-        ret = {}
-        for x in range(len(k)):
-            ret[k[x]] = y[x]
-        yield ret
+    k = kwargs.keys()
+    for v in izip(*kwargs.values()):
+        yield dict([(k[x],v[x]) for x in range(len(k))])
 
-def repeat_f(f, n=0, args=[], kwargs={}):
+def kwimap(f, *args, **kwargs):
+    '''
+    like imap, but pass kwargs as well.
+
+    Example:
+        def test(*args,**kwargs):
+            return "args: %s, kwargs: %s" % (repr(args),repr(kwargs))
+            
+        generator = iterutil.kwimap(test,
+            [1,2,3],
+            [4,5,6],
+            foo=['a','b','c'],
+            bar=['x','y','z'])
+
+        for result in generator:
+            print result
+
+        #OUTPUT
+
+        args: (1, 4), kwargs: {'foo': 'a', 'bar': 'x'}
+        args: (2, 5), kwargs: {'foo': 'b', 'bar': 'y'}
+        args: (3, 6), kwargs: {'foo': 'c', 'bar': 'z'}
+    '''
+    k = kwargs.keys()
+    for v in izip(*args+tuple(kwargs.values())):
+        map_ar = [v[x] for x in range(len(args))]
+        map_kw = dict([(k[x-len(args)],v[x]) for x in range(len(args),len(args)+len(k))])
+        yield f(*map_ar,**map_kw)
+    
+
+def repeat_f(f, n=None, args=[], kwargs={}):
     '''
     repeatedly calls function f, up to n times with arguments *args and **kwargs
     Like map, but repetitive
@@ -143,7 +229,7 @@ def repeat_f(f, n=0, args=[], kwargs={}):
 
     '''
     i = 0
-    while n == 0 or i < n:
+    while n is None or i < n:
         i+=1
         yield f(*args, **kwargs)
 
