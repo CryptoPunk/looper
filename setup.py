fc5a017a398871ce30df8396c7d34a3518bea882
syn cryptic <syn@foo.com>
2013-10-15 14:31:03 -0700

    Bumped version number for blackmamba support

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 
 setup(name='looper',
-      version='1.0',
+      version='1.1',
       description='A library for generating test cases',
       author='Max Vohra',
       author_email='max@seattlenetworks.com',
