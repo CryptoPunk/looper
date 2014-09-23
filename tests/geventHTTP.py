507364a1a817481b362d58a5810029733a90e42b
Max Vohra <max@seattlenetworks.com>
2014-09-23 02:40:43 -0700

    Added geventHTTP test

---
+++ tests/geventHTTP.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import sys,datetime,random
+from urlparse import urlparse
+sys.path.append('.')
+from looper import iterutil,clients
+
+params = iterutil.chain(
+    iterutil.dict_zip(
+        method = iterutil.repeat('GET'),
+        url = iterutil.concat(
+            iterutil.repeat('http://'),
+            iterutil.repeat('127.0.0.1'),
+            ["/uri","/uri1"],
+        ),
+        headers = iterutil.dict_zip({
+            'User-Agent': iterutil.repeat('SecurityInnovation/0.0.1/Looper'),
+            'Accept': iterutil.repeat("application/json"),
+            'Date': iterutil.repeat_f(lambda: datetime.datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")),
+            'X-Request-Id': iterutil.repeat_f(lambda:"%032x" % random.getrandbits(128)),
+            'Content-Type': iterutil.repeat('application/json'),
+        }),
+    ),
+)
+
+class Check(object):
+    '''
+    A simple HTTP response check
+    '''
+    def check(self,request,response):
+        print response
+
+class MyTestCase(Check,clients.geventHTTP.TestCase):
+    pass
+
+test = MyTestCase(params)
+test.run()
